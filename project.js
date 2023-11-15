ee30c650ceba1e3a6f0857ceee16d8c8eb19a6c3
dhisha25 <150920413+dhisha25@users.noreply.github.com>
2023-11-15 18:28:15 +0530

    Add files via upload

--- project.js
+++ project.js
@@ -4,12 +4,15 @@ import dog1 from "../images/dog1.jpg";
 function Project () {
     return (
 <div>
+    <h1>hi good morning</h1>
+    <h2>have a nice day</h2>
+<div>
 
      <img className="nature" src={nature}></img>
      <img className="dog" src={dog}></img>
     <picture> <img className="dog1" src={dog1}></img>
     </picture>
-
+</div>
 </div>
     );
 }
