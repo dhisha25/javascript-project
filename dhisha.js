4ec452bcc0d0795f310eb2396f5b7009b91ed44b
dhisha25 <150920413+dhisha25@users.noreply.github.com>
2023-11-17 18:36:14 +0530

    Add files via upload

--- dhisha.js
+++ dhisha.js
@@ -1,11 +1,20 @@
 import icon from "../images/react.jpg"
 import "../project/dhisha.css";
 function Vaishal (){
+    const day=["Monday", " Tuesday", " wednesday", " Thursday", " Friday", " saturday", " Sunday"]
     return(
         <div>
             <img className="java2" src={icon}></img>
             <h2 className="java3">React js</h2>
             <a href="/project">React is a programming language</a>
+            {day.length>0 &&
+    <h1>total {day.length} days</h1>}
+    
+    <h2 className="blue">{day.map((a)=>(a))}</h2>
+            
+
+
+
 
         </div>
     )
--- project.js
+++ project.js
@@ -2,15 +2,25 @@ import nature from "../images/nature.jpg";
 import dog from "../images/dog.jpg";
 import dog1 from "../images/dog1.jpg";
 import "../project/project.css";
-
-
 function Project () {
+   const fruit=["apple"," banana"," grapes"," watermelon"," pineapple"]
+  // const fruit=[]
     return (
 <div>
     <p className="java">javascript</p>
     <p className="java1">javascript is a programming language</p>
     <h1>hi good morning</h1>
     <h2>have a nice day</h2>
+    {fruit.length>0 &&
+    <h1 className="violet">total {fruit.length} fruits</h1>}
+    <h2 className="green">{fruit.map((a)=>(a))}</h2>
+    {fruit.length>0 ?(
+ <h3 className="yellow">I have a {fruit.length} fruits</h3>
+    ):(
+        <h3>no fruits</h3>
+    )
+   }
+    
 <div>
 
      <img className="nature" src={nature}></img>
