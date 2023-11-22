3cd27162d10bf5c525b3bd0b4d3765c050bb8622
dhisha25 <150920413+dhisha25@users.noreply.github.com>
2023-11-22 18:43:35 +0530

    Add files via upload

--- vaishal.js
+++ vaishal.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import nature from "../images/nature.jpg";
+import dog from "../images/dog.jpg";
+import dog1 from "../images/dog1.jpg";
 export default class Dhisha extends Component  {
     constructor(props){
         super(props);
@@ -10,6 +13,33 @@ export default class Dhisha extends Component  {
         return (
     <div>
         <h2>{this.state.js.map((a)=>(a))}</h2>
+        <img src={nature} class="img-thumbnail" width="390" height="236"></img>
+        <img src={dog} class="rounded" width="390" height="236"></img>
+        <img src={dog1} class="rounded-circle" width="390" height="236"></img>
+        <p class="text-success">I am dhisha and My hobby is reading books</p>
+        <p class="text-info">apple</p>
+        <p class="text-warning">banana</p>
+        <p class="text-warning">orange</p>
+        <p class="text-secondary">grapes</p>
+        <p class="text-body">pineapple</p>
+        <p class="text-body">kiwi</p>
+        <p class="text-light">carrot</p>
+        <p class="text-white">betroot</p>
+        <p class="text-primary">beans</p>
+        <br></br>
+        <p class="text-bg-dark">tomato</p>
+        <p class="text-bg-light">cabbage</p>
+        <p class="text-bg-secondary">onion</p>
+        <p class="text-bg-danger">broccoli</p>
+        <p class="text-bg-warning">capsicum</p>
+        <p class="text-bg-info">cauliflower</p>
+        <p class="text-bg-success">corn</p>
+        <p class="text-bg-primary">garlic</p>
+        <p>bittergourd</p>
+        <p>potato</p>
+        <p>pumpkin</p>
+        <p>mushroom</p>
+        <p>brinjal</p>
         <div class="container mt-5">
   <div class="row">
     <div class="col-md-4">
