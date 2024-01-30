103f3974aca549d3dc25ac320502b087e5e08e1f
dhisha25 <150920413+dhisha25@users.noreply.github.com>
2024-01-30 18:33:33 +0530

    Add files via upload

--- hooks.js
+++ hooks.js
@@ -1,4 +1,8 @@
 import React, { Component } from "react";
+import './barchart.css';
+import './hooks.css';
+ import axios from "axios";
+const ip="http://localhost:3000";
 class Hook extends Component{
     constructor(props){
         super(props);
@@ -11,6 +15,38 @@ class Hook extends Component{
             count:login.count+1
         }))
     }
+    getcall = async () => {
+        //api call to get all cpt data
+        const headers = {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${this.state.accesstoken}`,
+        };
+        // eslint-disable-next-line
+        const res = await axios
+          .get(`${ip}/head`, { headers: headers })
+          .then((res) => {
+            this.setState({
+              cptdata: res.data.cpt,
+            });
+          })
+          .catch((err) => {});
+      };
+      deletecall = async () => {
+        //api call to get all cpt data
+        const headers = {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${this.state.accesstoken}`,
+        };
+        // eslint-disable-next-line
+        const res = await axios
+          .delete(`${ip}/delete`, { headers: headers })
+          .then((res) => {
+            this.setState({
+              cptdata: res.data.cpt,
+            });
+          })
+          .catch((err) => {});
+      };
     decrement=()=>{
         this.setState((login)=>({
             count:login.count-1
@@ -22,16 +58,20 @@ class Hook extends Component{
             }))
         }
     }
+    componentDidMount() {
+        this.getcall();
+
+    }
     render(){
         return(
             <div>
-                <h1>Incerment using hooks</h1>
-                <p>count:{this.state.count}</p>
-                <button onClick={this.increment}>Incerment</button>
+                <h1 className="update">Incerment using hooks</h1>
+                <p className="circle">count:{this.state.count}</p>
+                <button onClick={this.increment} className="project">Incerment</button>
                 <br></br>
-                <h1>Decrement using hooks</h1>
-                <p>count:{this.state.count}</p>
-                <button onClick={this.decrement}>Decrement</button>
+                <h1 className="online">Decrement using hooks</h1>
+                <p className="chart-container">count:{this.state.count}</p>
+                <button onClick={this.decrement} className="hook">Decrement</button>
             </div>
         )
     }
